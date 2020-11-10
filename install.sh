afae226695738570885e6fe9ef4b6b7d6fca6042
Mark Lodato <mark@gnar.dog>
2020-11-10 09:42:44 -0500

    Add basic install script that might work

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cp ./.vimrc ~/.vimrc
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ./coc-settings.json ~/.config/nvim/coc-settings.json
+cp ./fish/config.fish ~/.config/fish/config.fish
+cp -r ./fish/functions ~/.config/fish/
