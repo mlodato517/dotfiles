204bab6b825554ffa9110a609be6fa818cff9a28
Mark Lodato <mlodato517@gmail.com>
2021-05-21 10:28:06 -0400

    Add .tmux.conf file (#10)

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 cp ./.vimrc ~/.vimrc
+cp ./.tmux.conf ~/.tmux.conf
 cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
 cp ./coc-settings.json ~/.config/nvim/coc-settings.json
 cp ./fish/config.fish ~/.config/fish/config.fish
